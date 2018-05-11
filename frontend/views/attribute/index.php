7b124033ae65e1865539294f4b4560916509e188
IrinaGavr <gavrJ2@yandex.ru>
2018-05-11 16:35:01 +0300

    комит

--- frontend/views/attribute/index.php
+++ frontend/views/attribute/index.php
@@ -1,15 +1,17 @@
 <?php
+
 use yii\helpers\Html;
 use common\models\Attribute;
 ?>
 
-<?php foreach ($attribute as $value): ?>
-    <ul>
-        <li>
-    <?= html::encode("{$value->name} ({$value->id})") ?>:
-    <? //= $value->population ?>
-    </li>
-    <?php endforeach; ?>
-    </ul>
 
-<?=\yii\widgets\LinkPager::widget(['pagination' => $pagination])?>
+<ul>
+        <?php foreach ($attribute as $value): ?>
+        <li>
+            <?= html::encode("{$value->name}") ?>:
+        <?= $value->id ?></li>
+<?php endforeach; ?>
+</ul>
+
+<?=
+\yii\widgets\LinkPager::widget(['pagination' => $pagination])?>
